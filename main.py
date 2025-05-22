00872ca0c09851e305a2ab9eeeffbb1c82e9fd3d
Shreyas-Kandel <shreyaskandel@gmail.com>
2025-05-22 07:27:21 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+def add_student():
+    print("Adding a student...")
+
+def update_student():
+    print("Updating a student...")
+
+def delete_student():
+    print("Deleting a student...")
+
+def view_students():
+    print("Viewing all students...")
+
+def take_attendance():
+    print("Taking attendance...")
+
+def view_attendance():
+    print("Viewing attendance...")
+
+def menu():
+    print("Welcome to the Student Attendance System \n")
+    print("Enter 1 to add a student \n")
+    print("Enter 2 to update a student \n")
+    print("Enter 3 to delete a student \n")
+    print("Enter 4 to view all students \n")
+    print("Enter 5 to Take attendance \n")
+    print("Enter 6 to view attendance \n")
+    print("Enter 7 to exit \n")
+
+while True:
+    menu()
+    option = input("Enter your choice: ")
+    if option == "1":
+        add_student()
+    elif option == "2":
+        update_student()
+    elif option == "3":
+        delete_student()
+    elif option == "4":
+        view_students()
+    elif option == "5":
+        take_attendance()
+    elif option == "6":
+        view_attendance()
+    elif option == "7":
+        print("Exiting the program...")
+        break
+    else:
+        print("Invalid option. Please try again.")
+
